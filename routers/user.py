e60c9d8ac44f4504a7afe46b324aed7f094aeaba
mykola <mykolaivanowski@mail.com>
2023-08-20 12:56:15 +0300

    put, delete

--- routers/user.py
+++ routers/user.py
@@ -10,10 +10,19 @@ router = APIRouter()
 
 @router.post(path='/', tags=["user"])
 async def create(data: user_dto.User = None, db: Session = Depends(get_db_connection)):
-    print("service post")
     return user_service.create_user(data, db)
 
 
 @router.get(path='/{id}', tags=["user"])
 async def get_user(id: str=None, db: Session = Depends(get_db_connection)):
     return user_service.get_user(id, db)
+
+
+@router.delete(path='/{id}', tags=['user'])
+async  def delete_usser(id: int = None, db: Session = Depends(get_db_connection)):
+    return user_service.delete_user(id, db)
+
+
+@router.put(path='/[id}', tags=['user'])
+async def update_user(id: int =None, data: user_dto.User = None, db: Session = Depends(get_db_connection)):
+    return user_service.update_user(data, db, id)
